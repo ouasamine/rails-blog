a4efa29f570fee838e912043c72eb5f8e3985344
ouasamine <ouas.amine@outlook.com>
2023-01-20 19:15:28 +0100

    Add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   after_create :update_posts_counter
+  validate :title, presence: true
+  validate :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_then_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_then_or_equal_to: 0 }
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
