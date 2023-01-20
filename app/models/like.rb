a6e6f81663d2990cd5d13187013999336ae2dc50
ouasamine <ouas.amine@outlook.com>
2023-01-20 01:19:17 +0100

    Auto-fix Rubocop errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
 
   def update_likes_counter
     Post.find(post.id).increment(:likes_counter)
