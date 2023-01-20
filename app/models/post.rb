5d2d30a51e0a178976bc80e8f0de38589ceac26a
ouasamine <ouas.amine@outlook.com>
2023-01-20 01:54:02 +0100

    Add active record callbacks to all methods & fix latest_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ApplicationRecord
+  after_create :update_posts_counter
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
 
   def update_posts_counter
-    User.find_by(name: author.name).increment(:posts_counter)
+    User.find_by(name: author.name).increment!(:posts_counter)
   end
 
   def latest_comments
-    Comment.find_by(post: self)
+    Comment.where(post: self).limit(5)
   end
 end
