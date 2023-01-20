1d8b7e36a8e1eae33c4ff76da676f53823a2e414
ouasamine <ouas.amine@outlook.com>
2023-01-20 21:58:44 +0100

    Create comment_spec and extend previous tests

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Post, type: :model do
     post = Post.create(title: nil)
     expect(post).to_not be_valid
   end
+
+  it 'update_posts_counter should increment posts_counter' do
+    user = User.create(name: 'Amine', posts_counter: 0)
+    post = Post.create(author: user, title: 'First post on the blog', comments_counter: 0, likes_counter: 0)
+    comment = Comment.create(post: post,text: 'Welcome!')
+    expect(user.posts_counter).to eql(1)
+  end
+
+  it 'latest_comments should return last comments' do
+    user = User.create(name: 'Amine', posts_counter: 0)
+    post = Post.create(title: 'First post on the blog', author: user, comments_counter: 0, likes_counter: 0)
+    first_comment = Comment.create(post: post, author: user, text: 'Welcome!')
+    second_comment = Comment.create(post: post,author: user, text: 'Hello!')
+    expect(post.latest_comments).to include(first_comment)
+  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
     user = User.create(name: nil)
     expect(user).to_not be_valid
   end
+
+  it 'latest_posts should return last posts' do
+    user = User.create(name: 'Amine', posts_counter: 0)
+    first_post = Post.create(author: user, title: 'First post on the blog', comments_counter: 0, likes_counter: 0)
+    second_post = Post.create(author: user, title: 'Second post on the blog', comments_counter: 0, likes_counter: 0)
+    expect(user.latest_posts).to include(first_post)
+  end
 end
