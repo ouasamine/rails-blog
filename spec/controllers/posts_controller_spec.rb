265073fd8cdd6cf75a27197a3cb582922129395d
ouasamine <ouas.amine@outlook.com>
2023-01-24 23:09:25 +0100

    Add more test cases to controllers specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'posts handling', type: :request do
+  it '/ response status should be ok' do
+    get '/users/:id/posts'
+    expect(response).to have_http_status(:ok)
+  end
+
   it '/ should render index' do
     get '/users/:id/posts'
     expect(response).to render_template(:index)
   end
+
+  it '/ page body should include given text' do
+    get '/users/:id/posts'
+    expect(response.body).to include("Here is a list of posts for agiven user")
+  end
+
+  it '/:id response status should be ok' do
+    get '/users/:id/posts/:id'
+    expect(response).to have_http_status(:ok)
+  end
+
   it '/:id should render show' do
     get '/users/:id/posts/:id'
     expect(response).to render_template(:show)
+  end
+
+  it '/:id page body should include given text' do
+    get '/users/:id/posts/:id'
+    expect(response.body).to include("Here is a single post of a given user")
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'users handling', type: :request do
+  it '/ response status should be ok' do
+    get '/users'
+    expect(response).to have_http_status(:ok)
+  end
+
   it '/ should render index' do
     get '/users'
     expect(response).to render_template(:index)
   end
+
+  it '/ page body should include given text' do
+    get '/users'
+    expect(response.body).to include("Here is a list of users")
+  end
+
+  it '/:id response status should be ok' do
+    get '/users/:id'
+    expect(response).to have_http_status(:ok)
+  end
+
   it '/:id should render show' do
     get '/users/:id'
     expect(response).to render_template(:show)
+  end
+  
+  it '/:id page body should include given text' do
+    get '/users/:id'
+    expect(response.body).to include("Here is details of a given user")
   end
 end
