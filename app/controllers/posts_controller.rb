cf1978c1265f53acf10c617fa8ef3f985154ee5c
ouasamine <ouas.amine@outlook.com>
2023-01-25 16:46:30 +0100

    Fetch posts details of given user & display them

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @single_user = User.find(params[:user_id].to_i)
+    @user_posts = Post.where(author: @single_user)
+  end
 
   def show; end
 end
