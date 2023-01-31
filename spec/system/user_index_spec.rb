f53e249d7866da39725a6c7e27238bcf17b70a7f
ouasamine <ouas.amine@outlook.com>
2023-01-31 11:14:28 +0100

    Create integration specs for user index page

--- spec/system/user_index_spec.rb
+++ spec/system/user_index_spec.rb
@@ -1,12 +1,27 @@
 require "rails_helper"
 
-RSpec.describe "Widget management", type: :system do
+RSpec.describe 'Users index page', type: :system do
   before do 
-    User.create(name: "Lilly", posts_counter: 0)
+    @user = User.create(name: "Lilly", posts_counter: 0)
   end
-  it "Visit users" do
+  it "should show users names" do
     visit users_path
-
     expect(page).to have_text("Lilly")
+  end
+
+  it 'should show users images' do
+    visit users_path
+    expect(page).to have_css('div.img-plch')
+  end
+
+  it 'should show users posts counters' do
+    visit users_path
+    expect(page).to have_content("Number Of Posts: #{@user.posts_counter}")
+  end
+
+  it 'should show users names' do
+    visit users_path
+    click_on "#{@user.name}"
+    expect(page).to have_current_path user_path(@user.id)
   end
 end
