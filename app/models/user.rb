81e8784abc44155b313b720d365888ba1c2a6c78
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-01 13:59:40 +0100

    Add attribute on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :confirmable
   validates :name, presence: true
   # validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
-  
+  attribute :posts_counter, :integer, default: 0
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
