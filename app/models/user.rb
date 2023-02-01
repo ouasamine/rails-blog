62f4077bae1d4590ff56a5ffac4f7beb83a52851
ouasamine <ouas.amine@outlook.com>
2023-02-01 13:53:23 +0100

    Add text field for user name in sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
   validates :name, presence: true
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  # validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
